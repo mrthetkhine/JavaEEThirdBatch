6e9a8765011ef2c4b4626f88565e039febbd5c87
Thet Khine <mrthetkhine@gmail.com>
2021-06-20 21:06:00 +0630

    Add Spring boot Project

---
+++ SpringCourse/src/main/java/com/javaeethirdbatch/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.javaeethirdbatch.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home()
+	{
+		System.out.println("Home Controller");
+		return "home";
+	}
+}
---
+++ SpringCourse/src/test/java/com/javaeethirdbatch/SpringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaeethirdbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
